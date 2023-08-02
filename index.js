59aa1d0faaaf6e240829c031b03aae0a81563edb
Satu Kontinen <satu.kontinen@edu.karelia.fi>
2023-08-02 14:35:49 +0300

    aaaa

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'))
 
 morgan.token('requestData', (req, res) => {
     return JSON.stringify(req.body);
