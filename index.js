a5d4852938b4dc6e9c804ebfa0550092c7f078ec
Satu Kontinen <satu.kontinen@edu.karelia.fi>
2023-08-07 12:05:42 +0300

    new version

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+const Contact = require('./models/person');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -5,8 +8,10 @@ const morgan = require('morgan');
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static('build'));
 
+
+//morgan
 morgan.token('requestData', (req, res) => {
     return JSON.stringify(req.body);
 });
@@ -14,103 +19,101 @@ morgan.token('requestData', (req, res) => {
 const customLogFormat = ':method :url :status :res[content-length] - :response-time ms :requestData';
 app.use(morgan(customLogFormat, { stream: { write: (message) => console.log(message.trim()) } }));
 
-let persons = [
-      {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      },
-      {
-        "name": "Elviira Kana",
-        "number": "040 1234567",
-        "id": 5
-      },
-      {
-        "name": "Pekka Kana",
-        "number": "040 7654321",
-        "id": 6
+//info sivu
+app.get('/api/info', (request, response, next) => {
+  const currentTime = new Date().toString();
+  Contact.find({})
+    .then(persons => {
+      response.send('Phonebook has info for ' + persons.length + ' people <p>' + currentTime);
+    })
+    .catch(error => next(error)); 
+});
+
+//kontaktien haku
+app.get('/api/persons', (request, response, next) => {
+  Contact.find({})
+      .then(persons => {
+          response.json(persons);
+      })
+      .catch(error => next(error)); 
+});
+
+//kontaktin haku id:llä
+app.get('/api/persons/:id', (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then(contact => {
+      if (contact) {
+        response.json(contact)
+      } else {
+        response.status(404).end()
       }
-    ]
-
-    app.get('/', (req, res) => {
-        res.send('<h1>Hello World!</h1>')
-     })
-      
-    app.get('/info', (req, res) => {
-        const currentTime = new Date().toString();
-        res.send('Phonebook has info for ' + persons.length + ' people <p>' + currentTime)
     })
+    .catch(error => next(error))
+})
 
-    app.get('/api/persons', (req, res) => {
-        res.json(persons)
+//uuden kontaktin lisääminen
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  const contact = new Contact({
+    name: body.name,
+    number: body.number,
+  })
+
+  contact.save()
+    .then(savedContact => {
+      response.json(savedContact)
     })
+    .catch(error => next(error));
+});
 
-    app.get('/api/persons/:id', (request, response) => {
-        const id = Number(request.params.id)
-        console.log(id)
-        const person = persons.find(person => person.id === id)
-
-        if (person) {
-            response.json(person);
-            console.log(person);
-        }
-        else {
-            response.status(404).json({ error: 'Person not found' });
-            console.log('error: Person not found');
-        }
-    });
-    
-    app.delete('/api/persons/:id', (request, response) => {
-        const id = Number(request.params.id)
-        persons = persons.filter(person => person.id !== id)
-        response.status(204).end()
+//kontaktin poisto
+app.delete('/api/persons/:id', (request, response, next) => {
+  Contact.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
-          
-    app.post('/api/persons', (request, response) => {
-        const body = request.body
-        const personExists = persons.find((person) => person.name === body.name);
-          
-        if (!body.name || !body.number) {
-            return response.status(400).json({error: 'name or number missing'})
-        }
+    .catch(error => next(error))
+})
 
-        if (personExists) {
-            return response.status(400).json({ error: 'name must be unique' });
-        }
-        
-        const person = {
-            name: body.name,
-            number: body.number,
-            id: generateId()
-        }
-          
-        persons = persons.concat(person)
-        response.json(person)
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Contact.findByIdAndUpdate(
+    request.params.id, 
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
+    .then(updatedContact => {
+      response.json(updatedContact)
     })
+    .catch(error => next(error))
+})
 
-    const generateId = () => {
-        const maxId = persons.length > 0
-            ? Math.max(...persons.map(n => n.id))
-            : 0
-        return maxId + 1
-    }
 
-    const PORT = process.env.PORT || 3001
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`)
-    })
+// error handling middlewares
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+app.use(errorHandler);
+
+
+//serverin käynnistys ja kuuntelu
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -26,7 +26,6 @@ if (process.argv.length === 3) {
         mongoose.connection.close()
     })
 }
-
 else if (process.argv.length === 5) {
 
     mongoose.connect(url)
